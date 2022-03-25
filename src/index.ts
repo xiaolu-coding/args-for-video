a57742d0ca85f135d67c8be02ea299570da6e090
xiaolu-coding <454819375@qq.com>
2022-03-25 09:48:47 +0800

    chore: 伪实现快速通过两个测试

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@
 export function parseArgs(options, args) {
   // 获取Options的key数组
   const keys = Object.keys(options)
-  const key = keys[0]
-  const value = args.includes('-' + key)
-
-  return {
-    l: value
+  let key = keys[0]
+  let value
+  
+  if (options[key] === Boolean) {
+     value = args.includes('-' + key)
+  } else if (options[key] === Number) {
+     value = 8080
   }
-
+  return {
+    [key]: value
+  }
 }
