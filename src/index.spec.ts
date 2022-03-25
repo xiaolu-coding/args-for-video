f179fc41384fd9b17f9ce4b31e71c4ad7e3d5a36
xiaolu-coding <454819375@qq.com>
2022-03-25 09:29:01 +0800

    chore: Tasking 任务拆分

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,19 +7,42 @@ import { add } from './index'
 // 需求分析
 // -l -p 8080 -d /usr/logs
 
-test('happy path', () => {
-  const options = {
-    l: Boolean,
-    p: Number,
-    d: String,
-  }
-  const args = parseArgs(options, ['-l', '-p', '8080', '-d', '/usr/logs'])
-  expect(args).toEqual({
-    l: true,
-    p: 8080,
-    d: '/usr/logs'
-  })
+// tasking 任务拆分
+// happy path
+// -l -> true
+
+test('当我们给定一个 -l 的时候， 应该返回 true', () => {
+
 })
+
+// -p -> 8080
+
+test.todo('当我们给定一个 -p 的时候，应该返回数字类型', () => {
+
+})
+// -d -> /usr/logs
+
+test.todo('当我们给定一个 -d 的时候，应该返回字符串类型', () => {
+
+})
+
+// sad path
+// -l r  
+// -p 8080 9090 
+
+// test('happy path', () => {
+//   const options = {
+//     l: Boolean,
+//     p: Number,
+//     d: String,
+//   }
+//   const args = parseArgs(options, ['-l', '-p', '8080', '-d', '/usr/logs'])
+//   expect(args).toEqual({
+//     l: true,
+//     p: 8080,
+//     d: '/usr/logs'
+//   })
+// })
 
 test('should', () => {
   expect(true).toBe(true)
