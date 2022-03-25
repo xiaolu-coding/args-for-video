aa27b92eedb1b8c8ba0a51b6a6056f82dacc692b
xiaolu-coding <454819375@qq.com>
2022-03-25 09:22:02 +0800

    chore: 定义接口实现方式

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,6 +4,23 @@ import { test, expect } from 'vitest'
 import { add } from './index'
 
 
+// 需求分析
+// -l -p 8080 -d /usr/logs
+
+test('happy path', () => {
+  const options = {
+    l: Boolean,
+    p: Number,
+    d: String,
+  }
+  const args = parseArgs(options, ['-l', '-p', '8080', '-d', '/usr/logs'])
+  expect(args).toEqual({
+    l: true,
+    p: 8080,
+    d: '/usr/logs'
+  })
+})
+
 test('should', () => {
   expect(true).toBe(true)
   expect(add(1, 2)).toBe(3)
