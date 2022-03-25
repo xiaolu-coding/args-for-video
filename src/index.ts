519a5c13370f75d1ed8234faf6ce50d192809687
xiaolu-coding <454819375@qq.com>
2022-03-25 09:40:54 +0800

    chore: 将伪实现的-l转为'-'+key

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 
 export function parseArgs(options, args) {
-
-  const value = args.includes('-l')
+  // 获取Options的key数组
+  const keys = Object.keys(options)
+  const key = keys[0]
+  const value = args.includes('-' + key)
 
   return {
     l: value
